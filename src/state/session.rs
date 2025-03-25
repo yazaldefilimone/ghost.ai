369fe55abbe0f0ee1be6109433ca2bb51c56bd0e
Yazalde Filimone <yazaldefilimon@gmail.com>
2025-03-25 21:59:40 +0200

    *

--- src/state/session.rs
+++ src/state/session.rs
@@ -12,17 +12,41 @@ pub struct Session {
 impl Session {
 	pub fn new() -> Self {
 		let engine = Engine::new();
-		let last_suggestion = String::new();
+		let last_suggestion = "auto complete com ghost.ai....".to_string();
 		let in_autocomplete = false;
 		let last_input_time = Instant::now();
 		Self { engine, last_suggestion, in_autocomplete, last_input_time }
 	}
 
+	pub fn is_empty(&self) -> bool {
+		self.engine.user_input.is_empty()
+	}
+
+	pub fn set_autocomplete(&mut self) {
+		self.in_autocomplete = true;
+	}
 	pub fn push_char(&mut self, c: char) {
 		self.engine.push_char(c);
 		self.last_input_time = Instant::now();
+		// self.in_autocomplete = false;
 	}
 
+	pub fn backspace(&mut self) -> Option<String> {
+		if self.in_autocomplete {
+			self.engine.in_suggestion = true;
+			self.engine.apply_suggestion(self.last_suggestion.as_str());
+			let text = self.engine.backspace();
+			self.engine.in_suggestion = false;
+			self.in_autocomplete = false;
+			return Some(text);
+		}
+		None
+	}
+
+	pub fn get_suggestion(&mut self) -> String {
+		self.in_autocomplete = true;
+		self.last_suggestion.clone()
+	}
 	pub fn reset(&mut self) {
 		self.engine.reset();
 		self.last_suggestion.clear();
