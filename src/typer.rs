9d090d15aa29eae3c319c51bfe012b658bbe5089
Yazalde Filimone <yazaldefilimon@gmail.com>
2025-03-25 21:59:40 +0200

    *

--- src/typer.rs
+++ src/typer.rs
@@ -1,12 +1,10 @@
 use enigo::{Enigo, Keyboard, Settings};
 
-//  todo: refactor....
 pub fn type_text(text: &str) {
 	let mut enigo = match Enigo::new(&Settings::default()) {
 		Ok(enigo) => enigo,
 		Err(_error) => {
 			eprintln!("Hmm...something seems to have gone wrong.");
-			// eprintln!("error: {:?}", error);
 			return;
 		}
 	};
@@ -15,7 +13,6 @@ pub fn type_text(text: &str) {
 		Ok(_) => {}
 		Err(_error) => {
 			eprintln!("Hmm...something seems to have gone wrong.");
-			// eprintln!("error: {:?}", error);
 		}
 	}
 }
